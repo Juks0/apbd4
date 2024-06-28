56bc7a7e7baadf34dea1cfeb19487b386212db06
Juks0 <129502580+Juks0@users.noreply.github.com>
2024-06-28 14:12:29 +0200

    added gitignore

--- LegacyAppConsumer/obj/Debug/net8.0/LegacyAppConsumer.AssemblyInfo.cs
+++ LegacyAppConsumer/obj/Debug/net8.0/LegacyAppConsumer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LegacyAppConsumer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa8abaa3d9c44f88f6863f24d3b6380c9d640bce")]
 [assembly: System.Reflection.AssemblyProductAttribute("LegacyAppConsumer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LegacyAppConsumer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
